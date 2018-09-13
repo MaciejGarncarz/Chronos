3f8fc8389159e813fa49c70fa7da75155ee60c9d
Maciej Garncarz <maciej.garncarz@microsoft.wsei.edu.pl>
2018-09-13 22:54:42 +0200

    Added enitity framework using code first and migrations.

---
+++ Chronos/Database/AppContext.cs
@@ -0,0 +1,21 @@
+using Chronos.Models;
+
+namespace Chronos.Database
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class AppContext : DbContext
+    {
+        public AppContext()
+            : base("name=AppContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<FinanceItem> FinanceItems { get; set; }
+    }
+}
