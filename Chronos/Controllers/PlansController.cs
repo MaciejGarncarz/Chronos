57858c07d9d68260fad64f3e220709485f8d2588
Maciej Garncarz <maciej.garncarz@microsoft.wsei.edu.pl>
2018-09-30 21:29:34 +0200

    Added controller for Plans

---
+++ Chronos/Controllers/PlansController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Chronos.Database;
+using Chronos.Models;
+
+namespace Chronos.Controllers
+{
+    public class PlansController : ApiController
+    {
+        private readonly Database.AppContext _context = new Database.AppContext();
+
+        // GET: api/Plans
+        public IQueryable<Plan> GetPlans()
+        {
+            return _context.Plans;
+        }
+
+        // GET: api/Plans/5
+        [ResponseType(typeof(Plan))]
+        public IHttpActionResult GetPlan(int id)
+        {
+            Plan plan = _context.Plans.Find(id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(plan);
+        }
+
+        // PUT: api/Plans/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPlan(int id, Plan plan)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != plan.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(plan).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlanExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Plans
+        [ResponseType(typeof(Plan))]
+        public IHttpActionResult PostPlan(Plan plan)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Plans.Add(plan);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = plan.Id }, plan);
+        }
+
+        // DELETE: api/Plans/5
+        [ResponseType(typeof(Plan))]
+        public IHttpActionResult DeletePlan(int id)
+        {
+            Plan plan = _context.Plans.Find(id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plans.Remove(plan);
+            _context.SaveChanges();
+
+            return Ok(plan);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PlanExists(int id)
+        {
+            return _context.Plans.Count(e => e.Id == id) > 0;
+        }
+    }
+}
